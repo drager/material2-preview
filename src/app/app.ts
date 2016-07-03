9374d736a0fb6189fad8397298d2d4fb7416358f
Jesper Håkansson <jesper@jesperh.se>
2016-07-03 12:27:02 +0200

    Move toolbar and sidenav from Home component to App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core'
-import {ROUTER_DIRECTIVES} from '@angular/router'
+import {Router, ROUTER_DIRECTIVES} from '@angular/router'
+import {MdButton} from '@angular2-material/button/button'
+import {MdIcon} from '@angular2-material/icon/icon'
+import {MdToolbar} from '@angular2-material/toolbar/toolbar'
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav/sidenav'
 
 @Component({
   selector: 'app',
   providers: [],
   pipes: [],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [MdButton, MdIcon, MdToolbar, MD_SIDENAV_DIRECTIVES, ROUTER_DIRECTIVES],
   templateUrl: 'app/app.html',
   styles: [require('./app.scss')],
 })
 export class App {
-  constructor() {}
+  componentsOpened: boolean = false
+
+  constructor(public router: Router) {}
+
+  toggleComponents() {
+    this.componentsOpened = !this.componentsOpened
+    return this.componentsOpened
+  }
 }
